ed9c39d6f68c1e91b2965b0cd3592f42c4d9df49
exotrezz <137353203+exotrezz@users.noreply.github.com>
2023-09-02 12:25:31 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     document.querySelector('.input-wrapper').addEventListener('click', () => {
         let inputWrapper = document.querySelector('.input-wrapper')
         let loginPlaceholder = document.querySelector('.login-placeholder')
@@ -22,12 +23,52 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     })
 
-    document.querySelector('.signin-button').addEventListener('click', () => {
+    document.querySelector('.verify-wrapper').addEventListener('click', () => {
+        let verWrapper = document.querySelector('.verify-wrapper')
+        let verPlaceholder = document.querySelector('.verify-placeholder')
+        let verify = document.querySelector('.verify-origin')
+        document.querySelector('.verify-wrapper').classList.add('active')
+        document.querySelector('.verify-wrapper > .verify-origin > span').style.color = '#6ABDF5'
+        verPlaceholder.style.display = 'none'
+        verify.style.display = 'block'
+        document.addEventListener('click', (event) => {
+            if (!verWrapper.contains(event.target)){
+                if (document.querySelector('.verify-origin > input').value.length !== 0){
+                    document.querySelector('.verify-wrapper').classList.remove('active')
+                    document.querySelector('.verify-wrapper > .verify-origin > span').style.color = '#536471'
+                }else{
+                    document.querySelector('.verify-wrapper').classList.remove('active')
+                    verPlaceholder.style.display = 'block'
+                    verify.style.display = 'none'
+                    document.querySelector('.verify-origin > input').value = ''
+                }
+            }
+        })
+    })
+
+    document.querySelector('.signin-button').addEventListener('click', (click) => {
+        click.preventDefault()
         if (document.querySelector('#login-input').value.length === 0){
             document.querySelector('.error-message').style.display = 'block'
             setTimeout(() => {
                 document.querySelector('.error-message').style.display = 'none'
             }, 3000)
+        }else{
+            document.getElementById('change-marker').innerHTML = document.getElementById('login-input').value
+            document.getElementById('defaultPage').style.display = 'none'
+            document.getElementById('globalLoader').style.display = 'block'
+            setTimeout(() => {
+                document.getElementById('globalLoader').style.display = 'none'
+                document.getElementById('verifyPage').style.display = 'block'
+            }, 3700)
         }
     })
 })
+
+function checkVer(value) {
+    if (value.length > 0 || value !== ''){
+        document.querySelector('.success-button').classList.remove('disabled')
+    }else{
+        document.querySelector('.success-button').classList.add('disabled')
+    }
+}
